cee24c0c4a2fe92a896ed456f856feb94040b686
Ricardo Sarapura <ricardo.sarapura@evertecinc.com>
2026-06-02 17:46:30 -0300

    Limites individuales

---
+++ US #417178 Task #493783 Limites individuales en cuentas DebitoPrepago Nuevas Tablas.sql
@@ -0,0 +1,173 @@
+﻿-------------------
+-- Nuevas tablas --
+-------------------
+
+-- Actualizar constraint de tipo de parámetro para permitir el nuevo tipo ACCOUNT_RELATION
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.check_constraints cc
+             JOIN sys.tables t ON t.object_id = cc.parent_object_id
+    WHERE t.name = 'C_PARAMETER'
+      AND cc.name = 'CKC_PARAMETER_TYPE_C_PARAME'
+      AND cc.definition LIKE '%PARAMETER_TYPE] = 10%'
+)
+    BEGIN
+        ALTER TABLE C_PARAMETER
+            DROP CONSTRAINT CKC_PARAMETER_TYPE_C_PARAME;
+
+        ALTER TABLE C_PARAMETER
+            ADD CONSTRAINT CKC_PARAMETER_TYPE_C_PARAME
+                CHECK (
+                    PARAMETER_TYPE = 10 OR PARAMETER_TYPE = 9 OR PARAMETER_TYPE = 8 OR
+                    PARAMETER_TYPE = 7 OR PARAMETER_TYPE = 6 OR PARAMETER_TYPE = 5 OR
+                    PARAMETER_TYPE = 4 OR PARAMETER_TYPE = 3 OR PARAMETER_TYPE = 2 OR
+                    PARAMETER_TYPE = 1
+                    );
+    END;
+
+-- Se inserta en la tabla C_PARAMETER el nuevo parámetro 'INDIVIDUAL_DEBIT_ACCOUNT_LIMITS'
+IF NOT EXISTS (
+    SELECT 1
+    FROM C_PARAMETER
+    WHERE CODE = 'INDIVIDUAL_DEBIT_ACCOUNT_LIMITS'
+)
+    BEGIN
+        INSERT INTO C_PARAMETER (
+            ID_PARAMETER,
+            PARAMETER_NAME,
+            CODE,
+            VALUE_TYPE,
+            PARAMETER_TYPE,
+            MODULE,
+            IS_CONFIGURED_BY_SCREEN
+        )
+        VALUES (
+                   (SELECT MAX(ID_PARAMETER) + 1 FROM C_PARAMETER),
+                   'Limites individuales por cuenta, persona y producto',
+                   'INDIVIDUAL_DEBIT_ACCOUNT_LIMITS',
+                   3,
+                   10,
+                   'ICS',
+                   1
+               );
+    END;
+
+-- Tabla de relación entre cuenta-persona-producto y parámetros de configuración
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.tables
+    WHERE object_id = OBJECT_ID(N'ICS_ACCOUNT_RELATION_PARAMETER')
+)
+    BEGIN
+        CREATE TABLE ICS_ACCOUNT_RELATION_PARAMETER
+        (
+            ID_PARAMETER        int            NOT NULL,
+            ID_ACCOUNT_RELATION int            NOT NULL,
+            VALID_FROM          datetime       NOT NULL,
+            VALID_TO            datetime       NULL,
+            DATE_VALUE          datetime       NULL,
+            STRING_VALUE        nvarchar(400)  NULL,
+            REAL_VALUE          decimal(18, 6) NULL,
+            INTEGER_VALUE       bigint         NULL,
+
+            CONSTRAINT PK_ICS_ACCOUNT_RELATION_PARAMETER
+                PRIMARY KEY (ID_PARAMETER, ID_ACCOUNT_RELATION, VALID_FROM),
+
+            CONSTRAINT FK_ICS_ACC_REL_PARAM_C_PARAMETER
+                FOREIGN KEY (ID_PARAMETER)
+                    REFERENCES C_PARAMETER (ID_PARAMETER),
+
+            CONSTRAINT FK_ICS_ACC_REL_PARAM_ACCOUNT_REL
+                FOREIGN KEY (ID_ACCOUNT_RELATION)
+                    REFERENCES ICS_ACCOUNT_RELATION (ID_ACCOUNT_RELATION)
+        );
+    END;
+
+-- Tabla de históricos
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.tables
+    WHERE object_id = OBJECT_ID(N'ICS_ACCOUNT_RELATION_PARAMETER_H')
+)
+    BEGIN
+        CREATE TABLE ICS_ACCOUNT_RELATION_PARAMETER_H
+        (
+            ID_ACCOUNT_RELATION_PARAMETER_H bigint IDENTITY(1,1) NOT NULL,
+            ID_ACCOUNT_RELATION             int                NOT NULL,
+            LIMIT_AMOUNT                    decimal(18,2)      NOT NULL,
+            VALID_FROM                      datetime           NOT NULL,
+            VALID_TO                        datetime           NULL,
+            CREATED_BY                      varchar(100)       NOT NULL,
+            CREATED_DATETIME                datetime           NOT NULL DEFAULT GETDATE(),
+            CLOSED_BY                       varchar(100)       NULL,
+            CLOSED_DATETIME                 datetime           NULL,
+
+            CONSTRAINT PK_ICS_ACCOUNT_RELATION_PARAMETER_H
+                PRIMARY KEY (ID_ACCOUNT_RELATION_PARAMETER_H),
+
+            CONSTRAINT FK_ICS_ACC_REL_PARAM_H_ACCOUNT_REL
+                FOREIGN KEY (ID_ACCOUNT_RELATION)
+                    REFERENCES ICS_ACCOUNT_RELATION (ID_ACCOUNT_RELATION),
+
+            CONSTRAINT CHK_ICS_ACC_REL_PARAM_H_LIMIT_AMOUNT
+                CHECK (LIMIT_AMOUNT > 0)
+        );
+    END;
+
+---------------
+-- Rollbacks --
+---------------
+
+-- 1. DROP tabla de históricos
+IF EXISTS (
+    SELECT 1
+    FROM sys.tables
+    WHERE object_id = OBJECT_ID(N'ICS_ACCOUNT_RELATION_PARAMETER_H')
+)
+    BEGIN
+        DROP TABLE ICS_ACCOUNT_RELATION_PARAMETER_H;
+    END;
+
+-- 2. DROP tabla de relación cuenta-persona-producto / parámetros
+IF EXISTS (
+    SELECT 1
+    FROM sys.tables
+    WHERE object_id = OBJECT_ID(N'ICS_ACCOUNT_RELATION_PARAMETER')
+)
+    BEGIN
+        DROP TABLE ICS_ACCOUNT_RELATION_PARAMETER;
+    END;
+
+-- 3. Eliminar parámetro INDIVIDUAL_DEBIT_ACCOUNT_LIMITS
+IF EXISTS (
+    SELECT 1
+    FROM C_PARAMETER
+    WHERE CODE = 'INDIVIDUAL_DEBIT_ACCOUNT_LIMITS'
+)
+    BEGIN
+        DELETE FROM C_PARAMETER
+        WHERE CODE = 'INDIVIDUAL_DEBIT_ACCOUNT_LIMITS';
+    END;
+
+-- 4. Revertir constraint CKC_PARAMETER_TYPE_C_PARAME
+IF EXISTS (
+    SELECT 1
+    FROM sys.check_constraints cc
+             JOIN sys.tables t ON t.object_id = cc.parent_object_id
+    WHERE t.name = 'C_PARAMETER'
+      AND cc.name = 'CKC_PARAMETER_TYPE_C_PARAME'
+      AND cc.definition LIKE '%PARAMETER_TYPE] = 10%'
+)
+    BEGIN
+        ALTER TABLE C_PARAMETER
+            DROP CONSTRAINT CKC_PARAMETER_TYPE_C_PARAME;
+
+        ALTER TABLE C_PARAMETER
+            ADD CONSTRAINT CKC_PARAMETER_TYPE_C_PARAME
+                CHECK (
+                    PARAMETER_TYPE = 9 OR PARAMETER_TYPE = 8 OR PARAMETER_TYPE = 7 OR
+                    PARAMETER_TYPE = 6 OR PARAMETER_TYPE = 5 OR PARAMETER_TYPE = 4 OR
+                    PARAMETER_TYPE = 3 OR PARAMETER_TYPE = 2 OR PARAMETER_TYPE = 1
+                    );
+    END;
+
---
+++ US #417178 Task #493783 Limites individuales en cuentas DebitoPrepago.sql
@@ -0,0 +1,77 @@
+﻿IF NOT EXISTS (SELECT 1 FROM TRD_SYSTEM_SERVICE WHERE INTERFACE_NAME = 'IIndividualDebitAccountLimitsService' AND OPERATION_NAME = 'GetCustomersByAccount')
+    BEGIN
+        INSERT INTO TRD_SYSTEM_SERVICE (INTERFACE_NAME, OPERATION_NAME, IS_IDEMPOTENT)
+        VALUES ('IIndividualDebitAccountLimitsService', 'GetActiveIndividualLimitByIssuerAndAccount', 0);
+    END;
+
+INSERT INTO TRD_SERVICE_PERM (INTERFACE_NAME, OPERATION_NAME, ID_ROLE, ID_PLATFORM, PERMISSION)
+SELECT 'IIndividualDebitAccountLimitsService', 'GetActiveIndividualLimitByIssuerAndAccount', ID_ROLE, 1, 3
+FROM TRD_ROLE r
+WHERE lower(r.ROLE_NAME) LIKE 'admin%'
+  AND NOT EXISTS (
+    SELECT 1 FROM TRD_SERVICE_PERM
+    WHERE INTERFACE_NAME = 'IIndividualDebitAccountLimitsService'
+      AND OPERATION_NAME = 'GetActiveIndividualLimitByIssuerAndAccount'
+      AND ID_ROLE = r.ID_ROLE
+      AND ID_PLATFORM = 1
+);
+
+
+IF NOT EXISTS (SELECT 1 FROM TVM_USE_CASE WHERE ID_USE_CASE = 'ICSUC078_IndividualDebitAccountLimits')
+    BEGIN
+        INSERT INTO TVM_USE_CASE (ID_USE_CASE, INTERFACE_NAME, OPERATION_NAME, DESCRIPTION, USE_CASE_TYPE, ENTITY_TYPE, ID_MODULE)
+        VALUES (
+                   'ICSUC078_IndividualDebitAccountLimits',
+                   'IIndividualDebitAccountLimitsService',
+                   'GetActiveIndividualLimitByIssuerAndAccount',
+                   'Gestionar limites individuales de cuenta debito-prepago',
+                   3,
+                   2,
+                   'ICS');
+    END;
+
+IF NOT EXISTS (SELECT 1 FROM TVM_USE_CASE_PLATFORM WHERE ID_PLATFORM = 1 AND ID_USE_CASE = 'ICSUC078_IndividualDebitAccountLimits')
+    BEGIN
+        INSERT INTO TVM_USE_CASE_PLATFORM (ID_PLATFORM, ID_USE_CASE)
+        VALUES (1, 'ICSUC078_IndividualDebitAccountLimits');
+    END;
+
+IF NOT EXISTS (SELECT 1 FROM TVM_MENU_ITEM WHERE ID_USE_CASE = 'ICSUC078_IndividualDebitAccountLimits')
+    BEGIN
+        INSERT INTO TVM_MENU_ITEM
+        VALUES (
+                   8,
+                   (SELECT MAX(ID_MENU_ITEM) + 1 FROM TVM_MENU_ITEM),
+                   'ICS',
+                   2,
+                   (SELECT ISNULL(MAX(MENU_ITEM_ORDER), 0) + 1
+                    FROM TVM_MENU_ITEM
+                    WHERE ID_FATHER_MI = (SELECT ID_MENU_ITEM FROM TVM_MENU_ITEM WHERE RESOURCE_KEY = 'MAINTENANCE_KEY' AND ID_CONTEXT_MENU = 8)),
+                   'ICSUC078_IndividualDebitAccountLimits',
+                   (SELECT ID_MENU_ITEM FROM TVM_MENU_ITEM WHERE RESOURCE_KEY = 'MAINTENANCE_KEY' AND ID_CONTEXT_MENU = 8),
+                   NULL,
+                   0);
+    END;
+
+
+SELECT ID_MENU_ITEM FROM TVM_MENU_ITEM WHERE RESOURCE_KEY = 'MAINTENANCE_KEY' AND ID_CONTEXT_MENU = 8;
+
+SELECT ISNULL(MAX(MENU_ITEM_ORDER), 0) + 1
+FROM TVM_MENU_ITEM
+WHERE ID_FATHER_MI = (SELECT ID_MENU_ITEM FROM TVM_MENU_ITEM WHERE RESOURCE_KEY = 'MAINTENANCE_KEY' AND ID_CONTEXT_MENU = 8)
+
+DELETE FROM TVM_MENU_ITEM
+WHERE ID_USE_CASE = 'ICSUC078_IndividualDebitAccountLimits';
+
+DELETE FROM TVM_USE_CASE_PLATFORM
+WHERE ID_USE_CASE = 'ICSUC078_IndividualDebitAccountLimits';
+
+DELETE FROM TVM_USE_CASE
+WHERE ID_USE_CASE = 'ICSUC078_IndividualDebitAccountLimits';
+
+DELETE FROM TRD_SERVICE_PERM
+WHERE INTERFACE_NAME = 'IIndividualDebitAccountLimitsService'
+  AND OPERATION_NAME = 'GetActiveIndividualLimitByIssuerAndAccount';
+
+DELETE FROM TRD_SYSTEM_SERVICE
+WHERE INTERFACE_NAME = 'IIndividualDebitAccountLimitsService' AND OPERATION_NAME = 'GetCustomersByAccount';
